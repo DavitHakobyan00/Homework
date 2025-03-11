2d29c69763afe4837697374098589e987fc9fa69
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-11 08:52:14 +0400

    Saved changes before pull

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,11 @@
+/** 1․Ստեղծել 2 փոփոխական(byte a և byte b ),  վերագրել
+ * կամայական արժեքներ և տպել  console
+ */
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+        System.out.println(a);
+        System.out.println(b);
+
+/** 2.
