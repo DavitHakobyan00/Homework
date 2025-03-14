43e58e1e58c27b58bc414294c44c80b9f84eb0c5
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-14 18:37:34 +0400

    class

---
+++ src/classObject/City.java
@@ -0,0 +1,21 @@
+package classObject;
+
+
+public class City {
+    public char firstLetter;
+    public int population;
+
+    public City (char letter, int population) {
+     public String firstLetter;
+     public String Population;
+    }
+
+    public static void main(String[] args) {
+
+        City yerevan = new City('Y', 1075000);
+        City moscow = new City('M', 12600000);
+        City newYork = new City('N', 8419000);
+        City paris = new City('P', 2148000);
+
+    }
+}
