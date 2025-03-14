1e85d5190ec8bd146f4ac6e0b56565427015db7e
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-14 16:12:59 +0400

    new

--- src/methods/Methods.java
+++ src/methods/Methods.java
@@ -1,29 +1,49 @@
-public class methods {
+package methods;
 
-    // 1.Write a method which has two input parameters:: type  int and return 0.
-    public static int methodWithTwoInts(int a, int b) {
-        return 0;
+public class Methods {
+
+    /**
+     *   1.Write a method which has two input parameters:: type  int and return 0.
+     */
+    public static float methodWithTwoInts(int a, int b) {
+        return 5.5f;
     }
 
-    // 2.Write a method which has  no input parameters   and returns nothing.
-    public static void methodWithoutParams() {
+    /**
+     * 2.Write a method which has  no input parameters   and returns nothing.
+     * @param a
+     * @param b
+     */
+    public static void methodWithoutParams(int a, int b ) {
         // Ничего не делает
+    return;
     }
 
-    // 3.Write a method which has boolean input parameters  and print “Hello World
+
+    /**
+     * 3.Write a method which has boolean input parameters  and print “Hello World"
+     * @param flag
+     */
     public static void methodWithBoolean(boolean flag) {
         System.out.println("Hello World");
     }
 
-    // 4.Write a method which has a char input parameter  and return this  input parameter.
+    /**
+     * 4.Write a method which has a char input parameter  and return this  input parameter.
+     * @param c
+     * @return
+     */
     public static char methodWithChar(char c) {
         return c;
     }
 
-    // 5.Write a method which has  two float input parameters  and return  one of them
+    /**
+     * 5.Write a method which has  two float input parameters  and return  one of them
+     * @param x
+     * @param y
+     * @return
+     */
     public static float methodWithFloats(float x, float y) {
         return x; // Можно вернуть y, в зависимости от условий
     }
-
-
 }
