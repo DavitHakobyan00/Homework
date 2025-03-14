9f09623d3f75930ee42e566122969b51c721fead
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-14 18:08:29 +0400

    store

--- src/classObject/store/Book.java
+++ src/classObject/store/Book.java
@@ -1,6 +1,13 @@
 package classObject.store;
 
-
 public class Book {
+    public String Title;
+    public String Author;
+    public static void main(String[] args) {
+        Book A1 = new Book();
+        A1.Title = "Horizon";
+        A1.Author = "McQueen";
+    }
+}
 
 
