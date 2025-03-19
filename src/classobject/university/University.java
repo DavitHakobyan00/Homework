d1ce2d02f74ca506ba8b579a06f1aa436a31ae48
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-19 23:53:21 +0400

    university

---
+++ src/classobject/university/University.java
@@ -0,0 +1,11 @@
+package  classobject.university;
+
+class university {
+    public String Name;
+    public String Faculty;
+    public static void main(String[] args) {
+        university a1 = new university();
+        a1.Name = "YSU";
+        a1.Faculty = "Law";
+    }
+}
