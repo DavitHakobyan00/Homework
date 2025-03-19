be32799c37b24e44071c2ebbe9fbf7fd13e2f03c
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-03-19 23:51:41 +0400

    student

---
+++ src/classobject/student/Student.java
@@ -0,0 +1,11 @@
+package classobject.student;
+
+class student {
+    public String Name;
+    public String Age;
+    public static void main(String[] args) {
+        student a1 = new student();
+        a1.Name = "Pavel";
+        a1.Age = "20";
+    }
+}
